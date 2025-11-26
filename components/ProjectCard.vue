9ad4ca557e1c227d5a70390f461ab947c85c92ec
Corentin Thomasset <33403840+corentinthomasset@users.noreply.github.com>
2025-11-26 10:18:04 -0500

    Added playinline to allow playback on mobile

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,5 +1,6 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import glitchPattern from '@/assets/media/glitch.png'
+
 const props = defineProps<{
   item: Project
   index: number
@@ -22,17 +23,17 @@ watch(
 
 <template>
   <div
-    class="bg-muted/20 border-border relative m-auto aspect-2/3 w-[90%] max-w-[900px] overflow-hidden rounded-3xl border md:aspect-3/2 dark:shadow-2xl"
     v-motion-slide-bottom
     :delay="150 + index * 100"
+    class="bg-muted/20 border-border relative m-auto aspect-2/3 w-[90%] max-w-[900px] overflow-hidden rounded-3xl border md:aspect-3/2 dark:shadow-2xl"
   >
-    <video v-if="item.thumbnailMedia" ref="video-el" class="aspect-2/3 w-full object-cover md:aspect-3/2" muted loop disablepictureinpicture :poster="item.thumbnailPoster">
+    <video v-if="item.thumbnailMedia" ref="video-el" :poster="item.thumbnailPoster" class="aspect-2/3 w-full object-cover md:aspect-3/2" disablepictureinpicture loop muted playinline>
       <source :src="item.thumbnailMedia" type="video/webm" />
     </video>
     <template v-else>
       <GlitchImage :img="glitchPattern" />
       <div class="absolute inset-0 flex items-center justify-center md:text-xl">
-        <EncryptedText text="this project is confidential" :speed="80" :pause-duration="3000" />
+        <EncryptedText :pause-duration="3000" :speed="80" text="this project is confidential" />
       </div>
     </template>
     <div class="absolute bottom-0 left-0 flex w-full flex-col items-start justify-between p-6 text-sm md:flex-row md:items-center">
@@ -43,7 +44,7 @@ watch(
       <p class="text-muted-foreground bg-background/10 rounded px-1 backdrop-blur-2xl">{{ item.date }}</p>
     </div>
   </div>
-  <div class="m-auto max-w-[900px] p-6 text-center" v-motion-fade :delay="200 + index * 100">
+  <div v-motion-fade :delay="200 + index * 100" class="m-auto max-w-[900px] p-6 text-center">
     <p class="text-muted-foreground text-center text-sm">{{ item.description }}</p>
     <ul class="inline-flex items-center gap-2 p-2">
       <li v-for="link in item.links" :key="link.url">
